3e8c87c7724f14fa91d7da8b4b04e582a75202cc
WT_FALL-25-26- <moonsaha721@gmail.com>
2025-11-17 11:44:30 +0600

    my commit

---
+++ Mid/Lab Task Submission/Lab Task 2/lab2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1> Student Registration</h1>
+        <table>
+            <tr>
+                <td>Full Name:</td>
+            </tr>
+            <tr>
+                <td><input type = "text"></td>
+            </tr>
+            <tr>
+                <td>
+                    Email:
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type = "text">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Password:
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type = "password">
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Confirm Passwrod:
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <button> Register</button>
+                </td>
+            </tr>
+            
+        </table>
+
+    </body>
+</html>
