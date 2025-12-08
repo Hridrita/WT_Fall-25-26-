28c52f7ea334559edae9591e6ffb73a0000fd81f
WT_FALL-25-26- <moonsaha721@gmail.com>
2025-12-08 12:24:51 +0600

    upload

---
+++ Final_lab_1/lab1.php
@@ -0,0 +1,22 @@
+<?php
+$employeeName ="moon";
+$employeeID = 1002;
+$salary = 45000;
+
+echo "Name: $employeeName <br>";
+echo "ID: $employeeID <br>";
+echo "Salary: $salary <br>";
+
+echo "Product List <br>"
+
+$products = ["bill", "software", "inventory"];
+
+foreach($products as $product){
+    echo "$product <br>";
+}
+
+?
+
+
+
+
